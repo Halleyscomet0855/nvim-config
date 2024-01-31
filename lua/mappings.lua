e8dde41c1906fb88475c68488fb8c625018fc75d
Halleyscomet08 <hunterxhalley@gmail.com>
2024-01-31 21:49:50 +0800

    Got the exit terminal keybind to work

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -17,3 +17,6 @@ vim.keymap.set("x", "<leader>p", "\"_dP")
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set("t", "<C-x>", "<C-\\><C-n>")
+
